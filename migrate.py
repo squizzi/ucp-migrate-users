25fb7747fef9b7b9d77fd22610b0c20d6883e325
Kyle Squizzato <kyle.squizzato@docker.com>
2018-03-06 14:58:42 -0500

    Finished import_accounts, verify_import, main
    Signed-off-by: Kyle Squizzato <kyle.squizzato@docker.com>

--- migrate.py
+++ migrate.py
@@ -9,7 +9,9 @@ import re
 import requests
 import logging
 import json
-from simplediff import diff
+import getpass
+from jsondiff import diff
+from time import sleep
 
 
 """ Yes or no prompting """
@@ -76,21 +78,13 @@ def get_token(username, password, url, retries=0):
 
 """
 Get a JSON dump of users or orgs from a given UCP URL, facilitated by the
-/accounts endpoint.  Specify users=True for users, False for orgs.
-Use customFilter for custom filters, if desired.
+/accounts/ endpoint.  Use customFilter for custom filters, if desired, else
+we will just pull 'all'.
 
 Then, ask the user to verify if the pulled information looks sane.
 """
-def get_accounts(authtoken, url, users=True, customFilter=None,
-                verifyUser=True):
+def get_accounts(authtoken, url, customFilter='all', verifyUser=True):
     headers = {"Authorization":"Bearer {0}".format(authtoken)}
-    # Setup filters
-    if customFilter != None:
-        filters = customFilter
-    if users:
-        filters = 'users'
-    else:
-        filters = 'orgs'
     logging.info('Generating a list of {0} from {1}...'.format(filters, url))
     # Make the request
     try:
@@ -102,9 +96,7 @@ def get_accounts(authtoken, url, users=True, customFilter=None,
     except requests.exceptions.RequestException as e:
         logging.error('Failed to get account list for UCP {0}: {1}'.format(url, e))
         retries+=1
-        retry_this('UCP', 10, retries, 3, get_accounts(authtoken, url
-                                                       users, customFilter,
-                                                       verifyUser))
+        retry_this('UCP', 10, retries, 3, get_accounts(authtoken, url, users, customFilter, verifyUser))
     logging.info('Getting ready to import the following accounts:\n{}'.format(r.text))
     # Using the captured information send a list of captured accounts out to
     # the user
@@ -125,34 +117,43 @@ def get_accounts(authtoken, url, users=True, customFilter=None,
 """
 Import a given JSON dump of accounts into a given UCP URL.
 """
-def import_accounts(authtoken, url, accountsJson):
-    headers = {"Authorization":"Bearer {0}".format(authtoken)}
+def import_accounts(authtoken, url, accountsJson, userPassword):
+    headers = {
+        "Authorization":"Bearer {0}".format(authtoken),
+        "Content-Type":"application/json"
+    }
     logging.info('Importing {0}... to {1}'.format(filters, url))
+    # Give each user a default configurable userPassword
+    if userPassword == None:
+        userPassword = 'changeme'
     # Iterate through the accounts schema and import each one
     x = 0
     for item in accountsJson["accounts"]:
-        toImport = accountsJson["accounts"][x]
-        # Grab just the account name for logging use later
-        accountName = toImport["name"]
+        # If the account is a user, update the dict with the password entry
+        if accountsJson["accounts"][x]["isOrg"] == False:
+            toImport = accountsJson["accounts"][x].update({u'password': userPassword})
+        else:
+            toImport = accountsJson["accounts"][x]
         # Get the extracted JSON into a format we can send via an HTTP request
         # that UCP will accept
+        # Grab just the account name for logging use later
+        accountName = toImport["name"]
+        # Add the account to the UCP
         try:
             r = requests.post(
                 url+'/accounts/',
                 headers=headers,
-                json=json.dumps(toImport),
+                data=json.dumps(toImport),
                 verify=False)
-        # If the account already exists just pass, but tell the user
-        except requests.exceptions.HTTPError as e:
-            if e.code == 404:
-                coolbeans
-        # If we get any form of timeout we'll just retry
-        except requests.exceptions.ConnectTimeout as e:
-            logging.error('Failed to add {0} account to UCP {1}: {2}'.format(
-                                                                      accountName,
-                                                                      url, e))
+        # If we get any form of exception from requests we'll just retry
+        except requests.exceptions.RequestException as e:
+            logging.error('Failed to add {0} account to UCP {1}: {2}'.format(accountName, url, e))
             retries+=1
             retry_this('UCP', 10, retries, 3, import_accounts(authtoken, url, accountsJson))
+        # If the account already exists just pass, but tell the user
+        if "ACCOUNT_EXISTS" in r.text:
+            logging.info('Cannot import {0}, account already exists'.format(accountName))
+            logging.debug(r.text)
         logging.debug('Imported: {}'.format(accountName))
         x+=1
     logging.info('All accounts successfully imported')
@@ -163,12 +164,24 @@ obtained from get_accounts() and the second which is freshly obtained using
 the authtoken against the UCP where import_accounts() was just ran, diff the
 json dumps to ensure all accounts were actually copied over.
 """
-def verify_import(authtoken, url, staleJson):
+def verify_import(authtoken, url, staleJson, customFilter='all'):
+    headers = {"Authorization":"Bearer {0}".format(authtoken)}
+    freshJson = get_accounts(authtoken, url, customFilter, verifyUser=False)
+    # If we detect differences then we'll log those and exit
+    # TODO: We don't really do anything more here, perhaps we can retry the
+    # differences?
+    diffedJson = diff(staleJson, freshJson)
+    if diffedJson != {}:
+        logging.error('Newly imported accounts list on {0} does not match existing {1} list'.format(url, customFilter))
+        logging.debug(diffedJson)
+        return False
+    else:
+        return True
 
 def main():
     # argument parsing
     parser = argparse.ArgumentParser(description='Generate a list of current \
-            users from a given Docker UCP and copy them over to a new UCP.')
+            accounts from a given Docker UCP and copy them over to a new UCP.')
     parser.add_argument("-i",
                         "--interactive",
                         dest="interactiveMode",
@@ -183,17 +196,21 @@ def main():
                         help="Provide a UCP url for the second UCP environment \
                         where users will be copied to.")
     parser.add_argument("--ucp-from-user",
-                        dest="ucpUser",
+                        dest="ucpUserOne",
                         help="UCP admin username to use for the --from UCP")
     parser.add_argument("--ucp-from-password",
-                        dest="ucpPassword",
+                        dest="ucpPasswordOne",
                         help="UCP admin password to use for the --from UCP")
     parser.add_argument("--ucp-to-user",
-                        dest="ucpUser",
+                        dest="ucpUserOne",
                         help="UCP admin username to use for the --to UCP")
     parser.add_argument("--ucp-to-password",
-                        dest="ucpPassword",
+                        dest="ucpPasswordOne",
                         help="UCP admin password to use for the --to UCP")
+    parser.add_argument("-P",
+                        "--user-password",
+                        dest="userPassword",
+                        help="The default password on newly imported user ")
     parser.add_argument("--skip-user-verify",
                         dest="skipVerify",
                         action="store_true",
@@ -204,6 +221,7 @@ def main():
                         dest="debug",
                         action="store_true",
                         help="Enable debugging")
+    #TODO: Implement customFilter support
 
     args = parser.parse_args()
 
@@ -216,6 +234,36 @@ def main():
         logger = logging.getLogger(name=None)
         logging.basicConfig(format='%(levelname)s: %(message)s',
                             level=logging.DEBUG)
+
+    # Ask user whether we should enter interactiveMode or not
+    if interactiveMode:
+        # build a list of inputs from the arguments and apply those to
+        # the argparse vars for backwards compatability
+        args.ucpOne = raw_input('Enter UCP where accounts will be copied FROM:')
+        args.ucpUserOne = raw_input('UCP admin username:')
+        args.ucpPasswordOne = getpass.getpass('UCP admin password')
+        args.ucpTwo = raw_input('Enter UCP where accounts will be copied TO:')
+        args.ucpUserTwo = raw_input('UCP admin username:')
+        args.ucpPasswordTwo = getpass.getpass('UCP admin password')
+        args.userPassword = raw_input('Enter the default password for imported user accounts:')
+    # If the user didn't prepend https:// to their UCP fqdn's we'll do it for
+    # them
+    if not "https://" in args.ucpOne:
+        args.ucpOne = "https://" + args.ucpOne
+    if not "https://" in args.ucpTwo:
+        args.ucpTwo = "https://" + args.ucpTwo
+    # Get an auth token against the --from UCP
+    ucpOneAuthtoken = get_token(args.ucpUserOne, args.ucpPasswordOne, args.ucpOne)
+    # Get a list of accounts against the --from UCP
+    ucpOneAccountJson = get_accounts(ucpOneAuthtoken, args.ucpOne, verifyUser=args.skipVerify)
+    # Get an auth token against the --to UCP
+    ucpTwoAuthToken = get_token(args.ucpUserTwo, args.ucpPasswordTwo, args.ucpTwo)
+    # Import accounts into the --to UCP
+    import_accounts(ucpTwoAuthToken, args.ucpTwo, ucpOneAccountJson, args.userPassword)
+    # Verify that everything looks good
+    verify_import(ucpTwoAuthToken, args.ucpTwo, ucpOneAccountJson)
+    # Tell the user we're done
+    logging.info('Complete.')
 
 """
 Main
